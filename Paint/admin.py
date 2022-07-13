a009374c63c483dcbae2780d7b6d2143f2593e79
Caitlyn1419 <1278206207@qq.com>
2022-07-13 23:13:54 +0800

    First commit

---
+++ Paint/admin.py
@@ -0,0 +1,18 @@
+# admin page
+# username: psypaint
+# password: psypaint
+
+from django.contrib import admin
+from Paint.models import testersData
+
+
+# Register your models here.
+# @admin.register(testersData)
+# class testersDateAdmin(admin.ModelAdmin):
+#     list_display = (
+#         'tester_Name', 'tester_Program', 'tester_Age', 'tester_Gender', 'q1', 'q2', 'q3', 'q4', 'q5', 'q6', 'q7', 'q8',
+#         'q9', 'q10', 'file_url', 'generate_time')
+
+
+# Register your models here.
+admin.site.register(testersData)
